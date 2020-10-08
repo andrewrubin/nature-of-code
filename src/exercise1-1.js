fbd1a2e666369a7a61882954bcf8eb155e2f8eb0
Andrew Rubin <andrew@wethecollective.com>
2020-10-08 16:17:14 -0700

    Add sine of delta time to manipulate walker size

--- src/exercise1-1.js
+++ src/exercise1-1.js
@@ -14,13 +14,14 @@ class Walker {
   constructor(ctx, x, y, size) {
     this.x = x;
     this.y = y;
-    this.width = size;
-    this.height = size;
+    this.size = size;
     this.ctx = ctx;
   }
 
-  update() {
+  update(delta) {
     const rand = Math.random();
+
+    // random direction
     if (rand < 0.25) {
       this.x++;
     } else if (rand < 0.5) {
@@ -30,10 +31,20 @@ class Walker {
     } else {
       this.y--;
     }
+
+    // grow/shrink width
+    this.size = Math.floor(Math.sin(delta * 0.001) * 5 + 6);
   }
 
   draw() {
-    this.ctx.fillRect(this.x, this.y, this.width, this.height);
+    this.ctx.fillStyle = "rgba(255,255,255,0.05)";
+    this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+
+    ctx.fillStyle = "black";
+    this.ctx.beginPath();
+    this.ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    this.ctx.closePath();
+    this.ctx.fill();
   }
 }
 
@@ -45,6 +56,6 @@ const w = new Walker(
   2
 );
 stage.onUpdate = (delta) => {
-  w.update();
+  w.update(delta);
   w.draw();
 };
